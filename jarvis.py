4b6e18a3172e85c77fe54564d7a2c0e549172e19
Megh Naik <meghnaik717@gmail.com>
2025-07-06 15:29:43 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -4,22 +4,6 @@ import pyttsx3
 import musicLibrary
 import requests
 from openai import OpenAI
-# from gtts import gTTS
-# import pygame
-# import os
-
-# def start_jarvis():
-#     try:
-#         with sr.Microphone() as source:
-#             speak("Listening...")
-#             audio = recognizer.listen(source)
-#         command = recognizer.recognize_google(audio)
-#         processCommand(command)
-#         return f"You said: {command}"
-#     except Exception as e:
-#         return f"Error: {e}"
-
-
 
 
 recognizer = sr.Recognizer()
@@ -31,32 +15,11 @@ def speak(text):
     engine.say(text)                    
     engine.runAndWait()
 
-# def speak_new(text):
-
-#     if isinstance(text, list):
-#         text = ' '.join(text)  # Join list elements into a single string
-
-#     tts = gTTS(text)
-#     tts.save("temp.mp3")
-
-#     pygame.mixer.init()
-
-#     pygame.mixer.music.load('temp.mp3')
-
-#     pygame.mixer.music.play()
-
-#     while pygame.mixer.music.get_busy():
-#         pygame.time.Clock().tick(10)
-    
-#     pygame.mixer.music.unload()
-
-#     os.remove("temp.mp3")
-    
 
 
 def aiProcess(command):
     client = OpenAI(
-    api_key="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",
+    api_key=" ",  #get key from open ai
     )
 
     completion = client.chat.completions.create(
@@ -84,7 +47,7 @@ def processCommand(c):
         link = musicLibrary.music[song]   
         webbrowser.open(link)
     elif "news" in c.lower():
-        r = requests.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=67300473a41342159d3a704b60db33dc")
+        r = requests.get(" ")   #news api key
 
         data = r.json()
 
@@ -96,20 +59,6 @@ def processCommand(c):
     else:
         output = aiProcess(c)
         speak(output)
-
-# def start_jarvis():
-#     try:
-#         with sr.Microphone() as source:
-#             print("Listening for command...")
-#             audio = recognizer.listen(source)
-
-#         command = recognizer.recognize_google(audio)
-#         processCommand(command)  
-#         return f"You said: {command}"  
-
-#     except Exception as e:
-#         return f"Error: {str(e)}"
-
 
 
 
